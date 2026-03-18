642d25848a721ab7350706511b72818846a78cd7
gustavo <gustavosilva.infocus@gmail.com>
2026-03-18 16:11:50 -0300

    feat: estrutura do banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import fastify from 'fastify';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 export const app = fastify();
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+prisma.user.create({
+  data: {
+    name: 'Gustavo',
+    email: 'gustavo@gmail.com',
+  },
+});
